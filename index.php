ec40008828740f8fdf42eb922e8d99262c933697
Marian Mdna <50773599+MarianMdna@users.noreply.github.com>
2022-03-10 11:53:05 -0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>practica github</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    este es un proyecto de prueba
+    </body>
+</html>
